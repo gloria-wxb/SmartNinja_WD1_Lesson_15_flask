ed5cee9b98d0919cd6098529287473e826497d07
Gloria WXB <gloria.weixelbaum@hotmail.com>
2019-11-09 11:03:52 +0100

    Jinja

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    some_text = "Hello Message!"
+    current_year = datetime.datetime.now().year
+    cities = ["Wien", "Salzburg", "Linz", "Graz"]
+    return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
 
 
 @app.route("/about")
